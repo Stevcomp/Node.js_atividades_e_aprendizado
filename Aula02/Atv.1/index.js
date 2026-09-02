540533ef57f030a4b25c494f5b2cca6f2eb26de7
Stvcomp <hub.stvcomp@gmail.com>
2026-09-02 15:25:21 -0300

    Upload Aula02, Atv.1

---
+++ Aula02/Atv.1/index.js
@@ -0,0 +1,23 @@
+const componenteOriginal = Object.freeze({
+  id: 17,
+  nome: 'Sensor ultrassônico',
+  estoque: 4,
+  bancada: 'Robótica'
+});
+
+const novoEstoque = Number(process.argv[2] || '12');
+
+function atualizarEstoque(componente, quantidade) {
+  if (!Number.isSafeInteger(quantidade) || quantidade < 0) {
+    throw new Error('Quantidade de estoque inválida.');
+  }
+  return { ...componente, estoque: quantidade };
+}
+
+try {
+  const componenteAtualizado = atualizarEstoque(componenteOriginal, novoEstoque);
+  console.log({ componenteOriginal, componenteAtualizado });
+} catch (erro) {
+  console.error(erro.message);
+  process.exitCode = 1;
+}
